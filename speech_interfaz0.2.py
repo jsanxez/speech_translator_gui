e2638dd443d7b57ecc919a637809eb93808c8b84
jsanchez <jsanxez7@gmail.com>
2020-09-14 02:16:22 -0500

    Texto de salida en docx con posibilidad de convertirlo en pdf

--- speech_interfaz0.2.py
+++ speech_interfaz0.2.py
@@ -4,9 +4,11 @@ import pyaudio
 import wave
 import time
 import speech_recognition as sr
-# pdf
-from reportlab.pdfgen import  canvas
-from reportlab.lib.pagesizes import A4
+# docx
+import docx
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+# 2pdf
+from docx2pdf import convert
 # Open file
 from tkinter import filedialog
 # Save as
@@ -27,30 +29,55 @@ class App():
         self.isrecording = True
         # nested frames
         self.frame1 = tk.Frame(main)
+        self.frame2 = tk.Frame(main)
+        self.frame3 = tk.Frame(main)
+        self.frame4 = tk.Frame(main)
 
         self.btn_explorer = tk.Button(self.frame1, text='Guardar como...', command=self.save_file)
         self.button1 = tk.Button(main, text='Grabar', bg='blue', fg='white', command=self.startrecording)
         self.button2 = tk.Button(main, text='Detener', bg='red', fg='black', command=self.stoprecording)
-        self.l1 = tk.Label(self.frame1, text="Documento de salida (.pdf): ")
-        # check
+        self.l1 = tk.Label(self.frame1, text="Documento de salida (.docx): ")
+        # checks
         self.state_var = tk.IntVar()
-        self.audio_state = tk.Checkbutton(main, text=" Renombrar audio?", variable=self.state_var)
-        self.entry_s = tk.Text(main, height= 5, width=50, wrap="word")
-        self.l2 = tk.Label(main, text="Asunto de la reunión: ")
+        self.audio_state = tk.Checkbutton(self.frame2, text=" Renombrar audio?", variable=self.state_var)
+        self.state_var2 = tk.IntVar()
+        self.pdf_state = tk.Checkbutton(self.frame2, text=" Generar pdf?", variable=self.state_var2)
+        # label and entry title
+        self.doc_title = tk.Label(self.frame3, text="Nombre del acta: ")
+        self.title_entry = tk.Entry(self.frame3, width=30)
+        # subject
+        self.l2 = tk.Label(self.frame4, text="Asunto de la reunión: ")
+        self.subject_box = tk.Text(self.frame4, height= 5, width=50, wrap="word")
+
         self.l3 = tk.Label(main, text="Estado...")
-        
+
+        # saveas
         self.l1.pack(side=tk.LEFT)
         self.l1.pack(padx=10, pady=5)
-        self.btn_explorer.pack(side=tk.RIGHT)
+        self.btn_explorer.pack(side=tk.LEFT)
         self.btn_explorer.pack(padx=10, pady=5)
-        # nested frame
-        self.frame1.pack(side=tk.TOP)
-
-        #check
+        # checks
+        self.audio_state.pack(side=tk.LEFT) 
         self.audio_state.pack(padx=10, pady=5)
-
+        self.pdf_state.pack(side=tk.LEFT)
+        self.pdf_state.pack(padx=10, pady=5)
+        # doc title
+        self.doc_title.pack(side=tk.LEFT)
+        self.doc_title.pack(padx=10, pady=5)
+        self.title_entry.pack(side=tk.LEFT)
+        self.title_entry.pack(padx=10, pady=5)
+        # subject
+        self.l2.pack()
         self.l2.pack(padx=10, pady=5)
-        self.entry_s.pack(padx=10, pady=5)
+        self.subject_box.pack()
+        self.subject_box.pack(padx=10, pady=5)
+        # nested frames
+        self.frame1.pack(side=tk.TOP)
+        self.frame2.pack(side=tk.TOP)
+        self.frame3.pack(side=tk.TOP)
+        self.frame4.pack()
+
+        self.l3.pack(padx=5, pady=5)
 
         self.button1.pack(side=tk.LEFT)
         self.button1.pack(padx=70, pady=5)
@@ -58,7 +85,6 @@ class App():
         self.button2.pack(side=tk.RIGHT)
         self.button2.pack(padx=70, pady=5)
 
-        self.l3.pack(padx=5, pady=5)
 
     def startrecording(self):
         self.p = pyaudio.PyAudio()  
@@ -71,9 +97,17 @@ class App():
 
         self.isrecording = True
 
-        # Agregando hora de inicio:
-        with open(self.file_path, 'w') as outfile:
-            outfile.write(time.strftime("Hora inicio: %d-%m-%Y %H:%M:%S\n"))
+        # Creando el documento
+        self.mydoc = docx.Document()
+        if not self.title_entry.get():
+            self.mydoc.add_heading("Reunion " + time.strftime("%d-%m-%Y"), 0)
+        else:
+            self.mydoc.add_heading(self.title_entry.get(), 0)
+
+        self.mydoc.add_paragraph(time.strftime("Fecha: %d/%m/%Y"))
+        self.mydoc.add_paragraph(time.strftime("Hora inicio: %H:%M:%S"))
+        self.mydoc.add_paragraph("Asunto: " + self.subject_box.get("1.0",
+        "end-1c"))
 
         print('Recording')
         self.l3.configure(text="Grabando...")
@@ -109,12 +143,18 @@ class App():
             audio = self.r.record(source)
             try:
                 text = self.r.recognize_google(audio, language='es-ES')
-                c = canvas.Canvas("hola_mundo.pdf", pagesize=A4)
-                c.drawString(0, 830, text)
-                c.save()
-                with open(self.file_path, 'a') as outfile:
-                    outfile.write(text)
-                    outfile.write(time.strftime("\nHora final: %d-%m-%Y %H:%M:%S"))
+                # Agregando la transcripcion al documento
+                self.mydoc.add_heading("Transcripción", 1)
+                my_paragraph = self.mydoc.add_paragraph(text)
+                my_paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+                self.mydoc.add_paragraph(time.strftime("Hora final: %H:%M:%S"))
+                        
+                # Guardando el archivo
+                self.mydoc.save(self.file_path)
+                # Generando el pdf
+                if self.state_var2.get() == 1:
+                    convert(self.file_path)
+
             except:
                 self.l3.configure(text="Error en el reconocimiento!")
         #---- fin de la supuesta funcion
@@ -136,16 +176,13 @@ class App():
         filename = filedialog.askopenfilename(
                 initialdir = ".",
                 title = "Select a file",
-                filetypes = (("PDF files",
-                ".pdf"),
-                ("all files",
-                "*.*")))
+                filetypes = (("docx files", ".docx"),
+                ("all files", "*.*")))
 
     def save_file(self):
         global file_path
-        files = [("All files", "*.*"),
-                ("PDF Files", "*.pdf"),
-                ("Text Document", "*.txt")]
+        files = [("Todos los archivos", "*.*"),
+                ("Documento de Word", "*.docx")]
         file = asksaveasfile(filetypes = files, defaultextension=files)
         self.file_path = file.name
 
@@ -153,7 +190,7 @@ class App():
 
 main = tk.Tk()
 main.title('Speech Translator')
-main.geometry('600x250')
+main.geometry('600x320')
 
 app = App(main)
 main.mainloop()
